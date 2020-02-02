ae4f115d73976bfe4415222a194bc985830571e6
maria vivanco <vivancomar@gmail.com>
2020-02-02 18:29:46 -0500

    cleaning, and parsing with different scenarios

--- cleaning.py
+++ cleaning.py
@@ -4,12 +4,25 @@ import string
 
 #to get the current working directory name
 docsFolder = os.getcwd() + "/docs"
+#token w only white space
 words = open("words.txt", "w+")
+#word seperation with punctuation 
+punctWords = open("punct.txt", "w+")
+#word seperation with downcase 
+downcaseWords = open("downCaseWords.txt", "w+")
+#word seperation with special punctuation
+specialPunctation = open("specialPuncWords.txt", "w+") 
+#word seperation with special puncutation and downcase 
+all = open("all.txt", "w+")
 
 #set up for acceptable/unacceptable punctuation 
 acceptablePunctuations = {"'", "-"}
 punctuation = ''.join((set(string.punctuation)).difference(acceptablePunctuations))
 translator = str.maketrans('', '', punctuation)
+allPunctTranslator = str.maketrans('', '', string.punctuation)
+
+#statistics 
+totalWords = 0
 
 def main():
     for filename in os.listdir(docsFolder):
@@ -18,10 +31,9 @@ def main():
             cleanFile(filePath)
         else:
             continue
-
 def cleanFile(file):
     #clean headers + ending
-    doc = open(file,"r")
+    doc = open(file, errors='ignore')
     for line in doc:
         header = "PREFACE" in line
         if header == True:  
@@ -33,8 +45,19 @@ def cleanFile(file):
         if "CHAPTER" in line: 
             continue 
         for word in line.split():
-            word = word.translate(translator)
+            #just white spaces 
             words.write(word + "\n")
-    
+            #special punctuation 
+            tempWord = word.translate(translator)
+            specialPunctation.write(tempWord + "\n")
+            #all punctutation 
+            temp =  word.translate(allPunctTranslator)
+            punctWords.write(temp + "\n")
+            #downcase 
+            tempWord = word.lower()
+            downcaseWords.write(tempWord + "\n")
+            #all scenarios
+            tem = (word.translate(translator)).lower()
+            all.write(tem + "\n")
 if __name__== "__main__":
   main()
